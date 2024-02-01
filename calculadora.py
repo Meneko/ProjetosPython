19536987a36f541813a5d8efc4ac0d6cf1c13001
Tiago Santos <pepinssantos@gmail.com>
2024-02-01 22:19:36 +0000

    1.2 i removed the "M+" and add like a normal calculator isn't perfect but i think is cool for now, i need to add comments explaining code and then maybeee in the future i add more functions, and comments(maybe tomorrow)
    1.1 now i think is more optimized than before, and now has 2 additional functions: AC - To erase data cache(Maybe in the future i go make more like a real calculator and not like a M+) and SAIR to exit of the program, and more error treatments!

--- calculadora.py
+++ calculadora.py
@@ -1,23 +1,35 @@
+import math
 class Calculadora:
-
     def __init__(self, operacao, n1, n2):
         self.operacao = operacao
         self.n1 = n1
         self.n2 = n2
 
     def returnoperacao(self):
-        if self.operacao == '+':
-            return Calculadora.soma(self)
-        elif self.operacao == '-':
-            return Calculadora.subtracao(self)
-        elif self.operacao == '*':
-            return Calculadora.multiplicacao(self)
-        elif self.operacao == '/':
-            return Calculadora.divisao(self)
+        match self.operacao:
+            case '+':
+                return self.n1 + self.n2
+
+            case '-':
+                return self.n1 - self.n2
+
+            case '/':
+                if self.n2 > 0:
+                    return self.n1 / self.n2
+                else:
+                    raise ZeroDivisionError('Não se pode dividir por zero')
+
+            case '*':
+                return self.n1 * self.n2
+
+            case 'sqrt':
+                return self.n1 + math.sqrt(self.n2)
+
+            case '^':
+                return self.n1 ** self.n2
 
     def validar(self):
-        operacoes = ['+', '-', '*', '/']
-        if self.operacao in operacoes:
+        if self.operacao in {'+', '-', '*', '/', 'sqrt', '^'}:
             pass
         else:
             raise Exception('A operação inserida é incorreta')
@@ -29,77 +41,102 @@ class Calculadora:
             raise ValueError('O numero inserido não é inteiro')
 
     def validarnum2(self):
-        if isinstance(self.n1, self.n2, int):
+        if isinstance(self.n2, int):
             pass
         else:
             raise ValueError('O numero inserido não é inteiro')
 
-    def soma(self):
-        return (self.n1 + self.n2)
-
-    def subtracao(self):
-        return (self.n1 - self.n2)
-
-    def divisao(self):
-        if self.n2 > 0:
-            return (self.n1 / self.n2)
-        else:
-            raise ZeroDivisionError('Não se pode dividir por zero')
-
-    def multiplicacao(self):
-        return (self.n1 * self.n2)
-
 
 def main():
-    ResultadoLocal = ResultadoGlobal
     print('Olá!, bem vindo a calculadora!')
     print('Introduza AC, para apagar tudo, e SAIR para sair!')
+    n1 = None
+    n2 = None
+    resultado = 0
     while True:
-        operacao = input('Insira uma operação presente nessa lista \'+\', \'-\', \'*\', \'/\'')
-        calculadora = Calculadora(operacao, n1=None, n2=None)
+        operacao = input('Insira uma operação presente nessa lista \'+\', \'-\', \'*\', \'/\', \'sqrt\', \'^\'> ')
+        calculadora = Calculadora(operacao.lower(), None, None)
         calculadora.validar()
 
-        n1 = input('Insira o primeiro numero')
-        try:
-            n1 = int(n1)
-        except:
-            if n1.upper() == 'SAIR':
-                break
-            elif n1.upper() == 'AC':
-                ResultadoLocal = ResultadoGlobal
-                print('TODOS OS ULTIMOS NUMEROS FORAM LIMPOS DO CACHE DO SISTEMA!')
-                continue
+        if operacao in {'+', '-', '*', '/', '^'}:
+            if not n1 is None:
+                pass
             else:
-                calculadora = Calculadora(operacao, n1, n2=None)
-                calculadora.validarnum1()
+                n1 = input('Insira o primeiro numero> ')
+                try:
+                    n1 = int(n1)
+                except:
+                    if n1.upper() == 'SAIR':
+                        break
+                    elif n1.upper() == 'AC':
+                        n1 = None
+                        n2 = None
+                        resultado = 0
+                        print('TODOS OS ULTIMOS NUMEROS FORAM LIMPOS DO CACHE DO SISTEMA!')
+                        continue
+                    else:
+                        calculadora = Calculadora(operacao, n1, None)
+                        calculadora.validarnum1()
 
-        n2 = input('Insira o segundo numero')
-        try:
-            n2 = int(n2)
-        except:
-            if n2.upper() == 'SAIR':
-                break
-            elif n2.upper() == 'AC':
-                ResultadoLocal = ResultadoGlobal
-                print('TODOS OS ULTIMOS NUMEROS FORAM LIMPOS DO CACHE DO SISTEMA!')
-                continue
+            if n2 is None:
+                n2 = input('Insira o segundo numero> ')
             else:
-                calculadora = Calculadora(operacao, n1, n2)
-                calculadora.validarnum2()
+                n2 = input('Insira o numero> ')
+            try:
+                n2 = int(n2)
+            except:
+                if n2.upper() == 'SAIR':
+                    break
+                elif n2.upper() == 'AC':
+                    n1 = None
+                    n2 = None
+                    resultado = 0
+                    print('TODOS OS ULTIMOS NUMEROS FORAM LIMPOS DO CACHE DO SISTEMA!')
+                    continue
+                else:
+                    calculadora = Calculadora(operacao, n1, n2)
+                    calculadora.validarnum2()
+            calculadora = Calculadora(operacao, n1, n2)
+            while True:
+                if resultado == 0:
+                    resultado = calculadora.returnoperacao()
+                    break
+                else:
+                    resultado = calculadora.returnoperacao() + n1
+                    break
+            print(f'{n1} {operacao} {n2} = \n{resultado}')
+            n1 = resultado
+        else:
+            n2 = input('Insira o numero da raiz quadrada> ')
+            try:
+                n2 = int(n2)
+            except:
+                if n2.upper() == 'SAIR':
+                    break
+                elif n2.upper() == 'AC':
+                    n1 = None
+                    n2 = None
+                    resultado = 0
+                    print('TODOS OS ULTIMOS NUMEROS FORAM LIMPOS DO CACHE DO SISTEMA!')
+                    continue
+                else:
+                    calculadora = Calculadora(operacao, n1, None)
+                    calculadora.validarnum1()
+            calculadora = Calculadora(operacao, 0, n2)
+            while True:
+                if resultado == 0:
+                    resultado = calculadora.returnoperacao()
+                    break
+                else:
+                    resultado = calculadora.returnoperacao() + n1
+                    break
+            print(f'√{n2} ≅ {round(resultado, 6)}')
+            n1 = resultado
 
-        calculadora = Calculadora(operacao, n1, n2)
-        resultado = calculadora.returnoperacao() + ResultadoLocal
-        print(f'{n1} {operacao} {n2} = \n{resultado}')
-        ResultadoLocal = resultado
+
 
     print('Obrigado por usar nossa calculadora =)')
 
 
-ResultadoGlobal = 0
-
 if __name__ == '__main__':
     main()
-
-
-def fim():
-    return 'Obrigado por usar nossa calculadora ='
